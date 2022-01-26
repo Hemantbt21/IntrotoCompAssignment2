580e32f76c07d51956f8af3b147447aec0bb92e9
Hemantbt21 <96773002+Hemantbt21@users.noreply.github.com>
2022-01-26 21:32:19 +0530

    Add files via upload

---
+++ output1.py
@@ -0,0 +1,11 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:\Users\DELL IN\Desktop\Code and stuff\asgn2ques1a.py =======
+Enter string:Python is a case sensitive language
+string length = 35
+string reversed = egaugnal evitisnes esac a si nohtyP
+sliced string= a case sensitive 
+string replaced = Python is object oriented language
+index of a = 10
+white_spaces_removed = Pythonisacasesensitivelanguage
---
+++ output4.py
@@ -0,0 +1,8 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:\Users\DELL IN\Desktop\Code and stuff\asgn2 ques4.py =======
+enter first no:7
+enter second no:4
+enter third no:9
+largest no is: 9
