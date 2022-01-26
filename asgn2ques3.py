b954a866e166c1c13ad832fa6e13cdaebe1fe596
Hemantbt21 <96773002+Hemantbt21@users.noreply.github.com>
2022-01-26 21:28:54 +0530

    Add files via upload

---
+++ asgn2ques3.py
@@ -0,0 +1,19 @@
+#ques3
+#a
+a=56
+b=10
+print(a&b)
+
+#b
+print(a|b)
+
+#c
+print(a^b)
+
+#d_left shift both a and b with 2 bits
+print(a<<2)
+print(b<<2)
+
+#e_right shift a with 2 bits and b with 4 bits
+print(a>>2)
+print(b>>4)
