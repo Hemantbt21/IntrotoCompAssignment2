18ceaf36ee31bef7c31f7c7e1a005e4cac802350
Hemantbt21 <96773002+Hemantbt21@users.noreply.github.com>
2022-01-26 23:13:00 +0530

    Delete asgn2ques5.py

--- asgn2ques5.py
+++
@@ -1,7 +0,0 @@
-#ques5_to check if a word entered by the user is present in the string
-x=str(input('Enter name:'))
-found='No'
-for i in['Tony','Liliv','Davy','Sentinal','Sam','Anna']:
-    if x==i:
-        found='Yes'
-print(found)
