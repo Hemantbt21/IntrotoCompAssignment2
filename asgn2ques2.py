25c260132fa82ab2768b03da27dd5092faea5e99
Hemantbt21 <96773002+Hemantbt21@users.noreply.github.com>
2022-01-26 21:28:05 +0530

    Add files via upload

---
+++ asgn2ques2.py
@@ -0,0 +1,12 @@
+#ques2
+name='Hemant'
+txt='Hey,%s Here!'%(name)
+print(txt)
+
+SID=21104119
+SID1='My SID is %s'%(SID)
+print(SID1)
+
+branchgrade='Electrical Engineering'
+branchgrade1='I am from %s department and my CGPA is 9.9'%(branchgrade)
+print(branchgrade1)
